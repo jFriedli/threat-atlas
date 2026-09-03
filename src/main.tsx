0a1dfbffed0f31433821967e40ccdff7f59e7c5a
jFriedli <12858685+jFriedli@users.noreply.github.com>
2026-09-03 10:44:05 +0200

    Fix boundary hit testing and resize interactions

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './milestones.css'
 import './snapshots.css'
+import './interaction.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
